05ce23e7f220c6e006369e4f660a8ab87c8fe622
Alan Christensen <alfy32@gmail.com>
2014-04-03 14:31:29 -0600

    changed batch file

---
+++ run3Agents.bat
@@ -0,0 +1,5 @@
+START project\BrilliantStudent\bin\Debug\BrilliantStudent.exe 52201
+
+START project\ExcuseGenerator\bin\Debug\ExcuseGenerator.exe 51301
+
+START project\WhiningSpinner\bin\Debug\WhiningSpinner.exe 51401
--- runAgents.bat
+++ runAgents.bat
@@ -1,17 +1,17 @@
-START project\BrilliantStudent\bin\Debug\BrilliantStudent.exe 1201
-START project\BrilliantStudent\bin\Debug\BrilliantStudent.exe 1202
-START project\BrilliantStudent\bin\Debug\BrilliantStudent.exe 1203
-START project\BrilliantStudent\bin\Debug\BrilliantStudent.exe 1204
-START project\BrilliantStudent\bin\Debug\BrilliantStudent.exe 1205
+START project\BrilliantStudent\bin\Debug\BrilliantStudent.exe 52201
+START project\BrilliantStudent\bin\Debug\BrilliantStudent.exe 52202
+START project\BrilliantStudent\bin\Debug\BrilliantStudent.exe 52203
+START project\BrilliantStudent\bin\Debug\BrilliantStudent.exe 52204
+START project\BrilliantStudent\bin\Debug\BrilliantStudent.exe 52205
 
-START project\ExcuseGenerator\bin\Debug\ExcuseGenerator.exe 1301
-START project\ExcuseGenerator\bin\Debug\ExcuseGenerator.exe 1302
-START project\ExcuseGenerator\bin\Debug\ExcuseGenerator.exe 1303
-START project\ExcuseGenerator\bin\Debug\ExcuseGenerator.exe 1304
-START project\ExcuseGenerator\bin\Debug\ExcuseGenerator.exe 1305
+START project\ExcuseGenerator\bin\Debug\ExcuseGenerator.exe 51301
+START project\ExcuseGenerator\bin\Debug\ExcuseGenerator.exe 51302
+START project\ExcuseGenerator\bin\Debug\ExcuseGenerator.exe 51303
+START project\ExcuseGenerator\bin\Debug\ExcuseGenerator.exe 51304
+START project\ExcuseGenerator\bin\Debug\ExcuseGenerator.exe 51305
 
-START project\WhiningSpinner\bin\Debug\WhiningSpinner.exe 1401
-START project\WhiningSpinner\bin\Debug\WhiningSpinner.exe 1402
-START project\WhiningSpinner\bin\Debug\WhiningSpinner.exe 1403
-START project\WhiningSpinner\bin\Debug\WhiningSpinner.exe 1404
-START project\WhiningSpinner\bin\Debug\WhiningSpinner.exe 1405
+START project\WhiningSpinner\bin\Debug\WhiningSpinner.exe 51401
+START project\WhiningSpinner\bin\Debug\WhiningSpinner.exe 51402
+START project\WhiningSpinner\bin\Debug\WhiningSpinner.exe 51403
+START project\WhiningSpinner\bin\Debug\WhiningSpinner.exe 51404
+START project\WhiningSpinner\bin\Debug\WhiningSpinner.exe 51405
